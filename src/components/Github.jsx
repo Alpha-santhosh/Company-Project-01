78c71388edb6cd4dc0ff846c3e4cb84192585003
santhosh s <subramanisanthosh00@gmail.com>
2023-04-22 22:33:07 +0530

    incomplete highcharts

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getSagaData } from '../redux/Action';
-import Chart from './Chart';
 
 function Github() {
   const gitRepoData = useSelector((state) => state.setData);
   const [data, setData] = useState([]);
-  const [CurrentPage, setCurrentPage] = useState(1);
-  const NumOfData = 10;
   const [page, setpage] = useState(1);
   const disptach = useDispatch();
   const [expand, setexpand] = useState('');
@@ -30,54 +27,82 @@ function Github() {
     }
   }, [gitRepoData]);
 
-  // const lastIndex = CurrentPage * NumOfData;
-  // const firstIndex = lastIndex - NumOfData;
-  // const dataToShow = data.slice(firstIndex, lastIndex);
-  // console.table(dataToShow);
   return (
-    (data.length > 1) ? (<div className="github">
-      {
-        data.map((e) => {
-          const { avatar, repoName, desp, stars, issue, lastPush, user_name } = e;
-          return <div className="card">
-            <div className="card_top">
-              <div className="right">
-                <img src={avatar} alt="" />
-              </div>
-              <div className="left">
-                <div className="top">
-                  <h2>{repoName}</h2>
-                  <p>{desp}</p>
+    (data.length > 1) ? (
+      <div className="github">
+        <h4>Current page : {page}</h4>
+        <br />
+        <div className="footer_btn">
+          {!((page - 1) === 0) ? (<button type='button' onClick={() => {
+            if (page > 1) {
+              setpage((page) => page - 1)
+              disptach(getSagaData(page - 1))
+            }
+          }}><a href='#app'>page {page - 1}</a></button>) : ('')}
+
+          <button type='button' onClick={() => {
+            setpage((page) => page + 1)
+            disptach(getSagaData(page + 1))
+          }}><a href='#app'>page {page + 1}</a></button>
+        </div>
+        <br />
+        {
+          data.map((e) => {
+            const { avatar, repoName, desp, stars, issue, lastPush, user_name } = e;
+            return <div className="card">
+              <div className="card_top">
+                <div className="right">
+                  <img src={avatar} alt="" />
                 </div>
-                <div className="btns">
-                  <span>
-                    Stars : {stars}
-                  </span>
-                  <span>
-                    Issues : {issue}
-                  </span>
+                <div className="left">
+                  <div className="top">
+                    <h2>{repoName}</h2>
+                    <p>{desp}</p>
+                  </div>
+                  <div className="btns">
+                    <span>
+                      Stars : {stars}
+                    </span>
+                    <span>
+                      Issues : {issue}
+                    </span>
+                  </div>
                   <div className='time'>Last pushed {lastPush} by {user_name}</div >
                 </div>
+                <div className="expand">
+                  <button type='button' className='drop_btn' style={(expand.name === repoName && expand.status === true) ? { transform: "rotate(0deg)" } : { transform: "rotate(-90deg)" }} onClick={() => {
+                    if (expand.name === repoName && expand.status === true) {
+                      setexpand({ name: repoName, status: false })
+                    } else {
+                      setexpand({ name: repoName, status: true })
+                    }
+                    console.log(repoName)
+                  }}><span class="material-symbols-outlined">
+                      expand_more
+                    </span></button>
+                </div>
               </div>
-              <div className="expand">
-                <button type='button' className='drop_btn' style={(expand.name === repoName && expand.status === true) ? { transform: "rotate(0deg)" } : { transform: "rotate(-90deg)" }} onClick={() => {
-                  if (expand.name === repoName && expand.status === true) {
-                    setexpand({ name: repoName, status: false })
-                  } else {
-                    setexpand({ name: repoName, status: true })
-                  }
-                  console.log(repoName)
-                }}><span class="material-symbols-outlined">
-                    expand_more
-                  </span></button>
-              </div>
+              {(expand.name === repoName && expand.status) ? (<div className="graph" style={(expand.name === repoName && expand.status) ? ({ display: "block" }) : ({ display: "none" })}>
+                {/* <Chart folder={repoName} owner={user_name} /> */}
+                <h4>HighChart is not working in github and also in vercel, During depolyment stage it's not supportting highcharts. <br />I am working on it soon it will be updated.... under development</h4>
+              </div>) : ('')}
             </div>
-            {(expand.name === repoName && expand.status) ? (<div className="graph" style={(expand.name === repoName && expand.status) ? ({ display: "block" }) : ({ display: "none" })}>
-              <Chart folder={repoName} owner={user_name} />
-            </div>) : ('')}
-          </div>
-        })
-      }</div>) : (<><h1>...Loading</h1></>)
+          })
+        }
+        <div className="footer_btn">
+          {!((page - 1) === 0) ? (<button type='button' onClick={() => {
+            if (page > 1) {
+              setpage((page) => page - 1)
+              disptach(getSagaData(page - 1))
+            }
+          }}><a href='#app'>page {page - 1}</a></button>) : ('')}
+
+          <button type='button' onClick={() => {
+            setpage((page) => page + 1)
+            disptach(getSagaData(page + 1))
+          }}><a href='#app'>page {page + 1}</a></button>
+        </div>
+      </div>) : (<><h1>...Loading</h1></>)
 
   )
 }
